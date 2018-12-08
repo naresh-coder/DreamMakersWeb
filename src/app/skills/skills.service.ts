bfbcc4bf601b8ecc3e0bbb666126f48eeee9f2d7
NareshD <nareshblr2015@gmail.com>
2018-12-08 09:05:49 +0530

    Update skills.service.ts

--- src/app/skills/skills.service.ts
+++ src/app/skills/skills.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import {Headers, Http, RequestOptions} from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import {Skills} from './skills';
 import 'rxjs/add/operator/map';
 
-
 @Injectable()
 export class SkillsService {
 
-  private url = 'http://localhost:8080/getSkills/';
+  private url = 'http://localhost:8080/getSkills';
+  private addUrl = 'http://localhost:8080/addSkills';
 
   constructor(private http: Http) {
   }
@@ -16,6 +16,16 @@ export class SkillsService {
   getSkills(): Observable<Skills> {
     return this.http.get(this.url)
       .map(res => res.json());
+  }
+
+  addSkill (skills: Skills): Observable<Skills>  {
+
+    const body = JSON.stringify({ skills });
+    const headers = new Headers({ 'Content-Type': 'application/json' });
+    const options = new RequestOptions({ headers: headers });
+
+    return this.http.post(this.addUrl, body, options)
+      .map(res =>  <Skills> res.json());
   }
 
 }
